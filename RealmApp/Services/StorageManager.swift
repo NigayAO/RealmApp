da41ebb7978f6270737973e1b3a6157fdaa9f4ba
Alik <Nigay_AO@bk.ru>
2021-12-12 12:23:55 +0300

    First attempt

---
+++ RealmApp/Services/StorageManager.swift
@@ -0,0 +1,85 @@
+//
+//  StorageManager.swift
+//  RealmApp
+//
+//  Created by Alexey Efimov on 08.10.2021.
+//  Copyright © 2021 Alexey Efimov. All rights reserved.
+//
+
+import RealmSwift
+
+class StorageManager {
+    static let shared = StorageManager()
+    
+    let realm = try! Realm()
+    
+    private init() {}
+    
+    // MARK: - Task List
+    func save(_ taskLists: [TaskList]) {
+        write {
+            realm.add(taskLists)
+        }
+    }
+    
+    func save(_ taskList: TaskList) {
+        write {
+            realm.add(taskList)
+        }
+    }
+    
+    func delete(_ taskList: TaskList) {
+        write {
+            realm.delete(taskList.tasks)
+            realm.delete(taskList)
+        }
+    }
+    
+    func edit(_ taskList: TaskList, newValue: String) {
+        write {
+            taskList.name = newValue
+        }
+    }
+
+    func done(_ taskList: TaskList) {
+        write {
+            taskList.tasks.setValue(true, forKey: "isComplete")
+        }
+    }
+
+    // MARK: - Tasks
+    func save(_ task: Task, to taskList: TaskList) {
+        write {
+            taskList.tasks.append(task)
+        }
+    }
+    
+    func delete(_ task: Task, from taskList: TaskList, by indexPath: IndexPath) {
+        write {
+            taskList.tasks.remove(at: indexPath.row)
+            realm.delete(task)
+        }
+    }
+    
+    func edit(_ task: Task, newValue: String) {
+        write {
+            task.name = newValue
+        }
+    }
+    
+    func done(_ task: Task) {
+        write {
+            task.setValue(!task.isComplete, forKey: "isComplete")
+        }
+    }
+    
+    private func write(completion: () -> Void) {
+        do {
+            try realm.write {
+                completion()
+            }
+        } catch {
+            print(error)
+        }
+    }
+}
